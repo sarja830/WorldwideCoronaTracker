3c270a7079eafe395be73a1b852fc7bc14621ae8
sarja830 <sarthjain830@gmail.com>
2020-07-03 18:25:51 +0530

    short project on react and axios

--- src/App.js
+++ src/App.js
@@ -7,78 +7,78 @@ import { fetchData } from './api'
 import image from './images/image.png'
 
 
-// const App = () => {
+const App = () => {
 
-//   const [data,setData]=useState({})
-//   const [country,setCountry] = useState('')
+  const [data,setData]=useState({})
+  const [country,setCountry] = useState('')
 
 
-//   useEffect(() => {
-//     const fetching = async()=>{
+  useEffect(() => {
+    const fetching = async()=>{
      
-//     setData(await fetchData())}
-//     fetching();
+    setData(await fetchData())}
+    fetching();
     
-//   }, [])
+  }, [])
 
 
-//   const handleCountryChange = async(country)=>{
-//    const data = await fetchData(country);
-//    setCountry(country)
-
-//    setData(data)
-//  }
-//    return (
-//     <div className={styles.container}>
-//     <img className={styles.image}  src={image}/>
-//       <Cards data={data}></Cards>
-       
-//        <CountryPicker handleCountryChange={handleCountryChange}></CountryPicker> 
-      
-//       <Charts data={data} country={country}></Charts>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-export class App extends Component {
- 
-
-  state={
-    data:{},
-    country:''
-  }
-
-  async componentDidMount(){
-    const fetchedData = await fetchData();
-    this.setState({data:fetchedData})
-
-    console.log(this.state.data)
-  }
-
-  handleCountryChange = async(country)=>{
+  const handleCountryChange = async(country)=>{
    const data = await fetchData(country);
+   setCountry(country)
 
- this.setState({data:data,country:country})
-  }
-
-
-  render() {
-    return (
-      <div className={styles.container}>
-      <img className={styles.image}  src={image}/>
-        <Cards data={this.state.data}></Cards>
-         
-         <CountryPicker handleCountryChange={this.handleCountryChange}></CountryPicker> 
-        
-        <Charts data={this.state.data} country={this.state.country}></Charts>
-      </div>
-    )
-  }
+   setData(data)
+ }
+   return (
+    <div className={styles.container}>
+    <img className={styles.image}  src={image}/>
+      <Cards data={data}></Cards>
+       
+       <CountryPicker handleCountryChange={handleCountryChange}></CountryPicker> 
+      
+      <Charts data={data} country={country}></Charts>
+    </div>
+  )
 }
 
 export default App
+
+
+
+
+// export class App extends Component {
+ 
+
+//   state={
+//     data:{},
+//     country:''
+//   }
+
+//   async componentDidMount(){
+//     const fetchedData = await fetchData();
+//     this.setState({data:fetchedData})
+
+//     console.log(this.state.data)
+//   }
+
+//   handleCountryChange = async(country)=>{
+//    const data = await fetchData(country);
+
+//  this.setState({data:data,country:country})
+//   }
+
+
+//   render() {
+//     return (
+//       <div className={styles.container}>
+//       <img className={styles.image}  src={image}/>
+//         <Cards data={this.state.data}></Cards>
+         
+//          <CountryPicker handleCountryChange={this.handleCountryChange}></CountryPicker> 
+        
+//         <Charts data={this.state.data} country={this.state.country}></Charts>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
